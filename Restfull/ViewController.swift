2ea69f02fe2a66505e9d05569c928a50746d98cb
Thien Le <thien.lequang@asnet.com.vn>
2018-04-08 17:17:37 +0700

    Decoable protocol for read request

--- Restfull/ViewController.swift
+++ Restfull/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 
 let DomainURL = "https://www.orangevalleycaa.org/api/"
 
-class Music {
+class Music: Codable {
   
-  var id:  String?
+  var guid:  String?
   var music_url: String?
   var name: String?
   var description: String?
+  
+  enum CodingKeys : String,  CodingKey {
+    case guid = "id"
+    case music_url, name, description
+  }
   
   static func fetch(withID id: Int) {
     let urlString = DomainURL + "music/id/\(id)"
@@ -23,16 +28,10 @@ class Music {
     if let url = URL.init(string: urlString) {
       let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
         print(String.init(data: data!, encoding: .ascii) ?? "no data")
-        if let objData = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) {
-          if let dict = objData as? [String: Any] {
-            let newMusic = Music()
-            newMusic.id = dict["id"] as? String
-            newMusic.name = dict["name"] as? String
-            newMusic.description = dict["description"] as? String
-            newMusic.music_url = dict["music_url"] as? String
-            print(newMusic)
-          }
+        if let newMusic = try?  JSONDecoder().decode(Music.self, from: data!) {
+          print(newMusic.guid ?? "nodata")
         }
+        
       })
       task.resume()
     }
